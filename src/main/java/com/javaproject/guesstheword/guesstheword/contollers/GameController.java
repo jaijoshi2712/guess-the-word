07f3f08867e62d7e9d2894a30883ed803f74fd94
jaijoshi2712 <jai.j2712@gmail.com>
2024-04-29 02:18:29 -0400

    feat: GameService created

--- src/main/java/com/javaproject/guesstheword/guesstheword/contollers/GameController.java
+++ src/main/java/com/javaproject/guesstheword/guesstheword/contollers/GameController.java
@@ -1,6 +1,8 @@
 package com.javaproject.guesstheword.guesstheword.contollers;
 
 
+import com.javaproject.guesstheword.guesstheword.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,9 +10,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class GameController {
 
+    @Autowired
+    GameService gameService;
+
 
     @GetMapping("/game-home")
     public String showGameHomePage(){
+        
+        String randomWord = gameService.toString();
         return "game-home-page";
     }
 
--- src/main/java/com/javaproject/guesstheword/guesstheword/service/GameService.java
+++ src/main/java/com/javaproject/guesstheword/guesstheword/service/GameService.java
@@ -1,6 +1,19 @@
 package com.javaproject.guesstheword.guesstheword.service;
 
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Random;
+
+@Service
 public class GameService {
 
-    
+    private String [] randomWords = {"father", "mother", "sister", "string", "hello", "light", "java"};
+
+    Random random = new Random();
+
+    @Override
+    public String toString() {
+        return randomWords[random.nextInt(randomWords.length)];
+    }
 }
