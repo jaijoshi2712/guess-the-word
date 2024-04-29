e75c82c0cc1b2b21a087a420f3b32b935122229b
jaijoshi2712 <jai.j2712@gmail.com>
2024-04-29 03:00:31 -0400

    feat: word hiding logic added

--- src/main/java/com/javaproject/guesstheword/guesstheword/contollers/GameController.java
+++ src/main/java/com/javaproject/guesstheword/guesstheword/contollers/GameController.java
@@ -4,6 +4,7 @@ package com.javaproject.guesstheword.guesstheword.contollers;
 import com.javaproject.guesstheword.guesstheword.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,9 +16,10 @@ public class GameController {
 
 
     @GetMapping("/game-home")
-    public String showGameHomePage(){
-        
+    public String showGameHomePage(Model model){
+
         String randomWord = gameService.toString();
+        model.addAttribute( "wordToDisplay", randomWord);
         return "game-home-page";
     }
 
--- src/main/java/com/javaproject/guesstheword/guesstheword/service/GameService.java
+++ src/main/java/com/javaproject/guesstheword/guesstheword/service/GameService.java
@@ -8,12 +8,28 @@ import java.util.Random;
 @Service
 public class GameService {
 
+    private String randomlyChoosenWord = null;
     private String [] randomWords = {"father", "mother", "sister", "string", "hello", "light", "java"};
-
+    private char [] allCharactersOfTheWord;
     Random random = new Random();
+
+    public GameService() {
+        randomlyChoosenWord = randomWords[random.nextInt(randomWords.length)];
+        allCharactersOfTheWord = new char[randomlyChoosenWord.length()];
+    }
 
     @Override
     public String toString() {
-        return randomWords[random.nextInt(randomWords.length)];
+
+        String ret = "";
+
+        for(char c: allCharactersOfTheWord){
+            if (c=='\u0000'){
+                ret = ret+ "_";
+            }
+            ret = ret + ' ';
+        }
+
+        return ret;
     }
 }
