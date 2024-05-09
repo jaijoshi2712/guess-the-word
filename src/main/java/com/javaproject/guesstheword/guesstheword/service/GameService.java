5cafe36c101afb2808ab8ec0412e615e7d19ebde
Aman <54438862+amanboobna@users.noreply.github.com>
2024-05-08 22:43:06 -0400

    added UI and single alphabet checks

--- src/main/java/com/javaproject/guesstheword/guesstheword/service/GameService.java
+++ src/main/java/com/javaproject/guesstheword/guesstheword/service/GameService.java
@@ -1,22 +1,42 @@
 package com.javaproject.guesstheword.guesstheword.service;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Random;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
-
-import java.util.Arrays;
-import java.util.Random;
 
 @Service
 @Scope("prototype")
 public class GameService {
 
     private String randomlyChoosenWord = null;
-    private String [] randomWords = {"father", "mother", "sister", "string", "hello", "light", "java"};
+    // private String [] randomWords = {"father", "mother", "sister", "string", "hello", "light", "java"};
     private char [] allCharactersOfTheWord;
     Random random = new Random();
 
     public GameService() {
-        randomlyChoosenWord = randomWords[random.nextInt(randomWords.length)];
+        HttpRequest request = HttpRequest.newBuilder()
+        .uri(URI.create("https://random-word-api.herokuapp.com/word?length=5"))
+        // https://random-word-api.herokuapp.com/word?number=10&length=5
+		.method("GET", HttpRequest.BodyPublishers.noBody())
+		.build();
+    HttpResponse<String> response=null;
+    try {
+        response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+    } catch (IOException e) {
+        e.printStackTrace();
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+    System.out.println("Length of Response body " +response.body());
+    String responseString = response.body().toString();
+        randomlyChoosenWord =responseString.substring(2, responseString.length() - 2);
+        // randomlyChoosenWord = randomWords[random.nextInt(randomWords.length)];
         allCharactersOfTheWord = new char[randomlyChoosenWord.length()];
     }
 
